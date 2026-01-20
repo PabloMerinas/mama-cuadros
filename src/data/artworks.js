b543556f4ac2ebcc041440dcb000b4f29abfea30
Pablo Merinas <pablomersot@gmail.com>
2026-01-20 12:12:17 +0100

    .

--- src/data/artworks.js
+++ src/data/artworks.js
@@ -1,21 +1,21 @@
 export const artworks = [
   {
     title: "Rojo Intenso",
-    image: "./obras/cuadro1.png",
+    image: "/obras/cuadro1.png",
     technique: "Óleo",
     size: "80×60 cm",
     year: 2024,
   },
   {
     title: "Azul Noche",
-    image: "./obras/cuadro2.png",
+    image: "/obras/cuadro2.png",
     technique: "Acrílico",
     size: "70×50 cm",
     year: 2023,
   },
   {
     title: "Luz y Sombra",
-    image: "./obras/cuadro3.png",
+    image: "/obras/cuadro3.png",
     technique: "Mixta",
     size: "60×60 cm",
     year: 2024,
